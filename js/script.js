b66e1175fcc6429200ec36571e8c574075bd33d8
ragnarsr <ragnar.schon@sverigesradio.se>
2017-01-03 14:35:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,78 @@
+window.onload = init;
+var context;
+var bufferLoader;
+
+function init() {
+  // Fix up prefixing
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  context = new AudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      '../sounds/hyper-reality/br-jam-loop.wav',
+      '../sounds/hyper-reality/laughter.wav',
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source1.start(0);
+  source2.start(0);
+}
 
 
-var kanal = 33; // Default channel
-var url = "http://api.sr.se/api/v2/channels/?liveaudiotemplateid=2&audioquality=hi&format=json&indent=true&pagination=false";
-
-var request = new XMLHttpRequest();
-request.open('GET', url, true);
-request.onload = function() {
-	var data = JSON.parse(request.response);
-	
-	var channelArray = [];
-	for (i=0; i < data.channels.length; i++) {
-		if (data.channels[i].channeltype != 'Extrakanaler') {
-			channelArray.push("<li data-toggle='collapse' data-target='#collapsable-nav'><a href='javascript:;' onclick='changeChannel("+i+")'>"+data.channels[i].name+"</a></li>");
-		}
-	}
-
-	document.getElementById('nav-list').innerHTML = channelArray.join(""); // Get rid of commas and insert list
-
-	var channelData = data.channels[kanal];
-
-	document.getElementById('pic-show').style.background = 'url('+channelData.image+')';
-	document.getElementById('pic-show').style.backgroundSize = '100%';
-	var audio = document.getElementById('audioSource');
-	audio.src = channelData.liveaudio.url;
-	document.getElementById('programTitle').innerHTML = channelData.name;
-};
-request.send();
-
-var yourAudio = document.getElementById('yourAudio'),
-    ctrl = document.getElementById('audioControl');
-
-ctrl.onclick = function () {
-	if(ctrl.className == "glyphicon glyphicon-play") {
-		yourAudio.load();
-        ctrl.className="glyphicon glyphicon-pause";
-        yourAudio["play"]();
-	}
-	else if (ctrl.className == "glyphicon glyphicon-pause"){
-	ctrl.className="glyphicon glyphicon-play";
-	    yourAudio["pause"]();
-	}
+var VolumeSample = {
 };
 
-function changeChannel (channel) {
-	var request = new XMLHttpRequest();
-	request.open('GET', url, true);
-	request.onload = function() {
-		var data = JSON.parse(request.response);
- 			var channelData = data.channels[channel];
-		document.getElementById('pic-show').style.background = 'url('+channelData.image+')';
-		document.getElementById('pic-show').style.backgroundSize = '100%';
-		var audio = document.getElementById('audioSource');
-		document.getElementById('programTitle').innerHTML = channelData.name;
-		audio.src = channelData.liveaudio.url;
-		yourAudio.load();
-		if(ctrl.className == "glyphicon glyphicon-play") {
-			yourAudio.load();
-		}
-		else if (ctrl.className == "glyphicon glyphicon-pause"){
-			yourAudio.load();
-			yourAudio.play();
-		}
-	};
-	request.send();
+// Gain node needs to be mutated by volume control.
+VolumeSample.gainNode = null;
+
+VolumeSample.play = function() {
+  if (!context.createGain)
+    context.createGain = context.createGainNode;
+  this.gainNode = context.createGain();
+  var source = context.createBufferSource();
+  source.buffer = BUFFERS.techno;
+
+  // Connect source to a gain node
+  source.connect(this.gainNode);
+  // Connect gain node to destination
+  this.gainNode.connect(context.destination);
+  // Start playback in a loop
+  source.loop = true;
+  if (!source.start)
+    source.start = source.noteOn;
+  source.start(0);
+  this.source = source;
+};
+
+VolumeSample.changeVolume = function(element) {
+  var volume = element.value;
+  var fraction = parseInt(element.value) / parseInt(element.max);
+  // Let's use an x*x curve (x-squared) since simple linear (x) does not
+  // sound as good.
+  this.gainNode.gain.value = fraction * fraction;
+};
+
+VolumeSample.stop = function() {
+  if (!this.source.stop)
+    this.source.stop = source.noteOff;
+  this.source.stop(0);
+};
+
+VolumeSample.toggle = function() {
+  this.playing ? this.stop() : this.play();
+  this.playing = !this.playing;
 };
